9825b49885bcab04b2ff19f6f6ba25b11b529111
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2024-02-27 09:40:51 +0700

    ok

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validate->fails()) {
+            $respon = [
+                'status' => 'error',
+                'msg' => 'Validator error',
+                'errors' => $validate->errors(),
+                'content' => null,
+            ];
+            return response()->json($respon, 400);
+        } else {
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                $respon = [
+                    'status' => 'error',
+                    'msg' => 'Unathorized',
+                    'errors' => null,
+                    'content' => null,
+                ];
+                return response()->json($respon, 401);
+            }
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception("Error in login");
+            }
+            $tokenResult = $user->createToken('token-auth')->plainTextToken;
+            $respon = [
+                'status_code' => 200,
+                'success' => true,
+                'data' => [
+                    'Nama' => $request->user()->name,
+                    'access_token' => $tokenResult,
+                    'token_type' => 'Bearer',
+                    'user_id' => $request->user()->id,
+                ]
+            ];
+            return response()->json($respon, 200);
+        }
+    }
+
+   
+}
+
+
+?>
